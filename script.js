8badb2ef4db52a3507640a39e89c0a6a447b1f0f
jtoledom1 <jtoledom1@outlook.com>
2024-10-23 16:32:25 -0600

    😅 not perspecgtive implement xd

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', () => {
-    const scrollPosition = window.scrollY;
-    const textElement = document.getElementById('text');
-    
-    // Aplica una transformación en 3D basada en la posición del scroll
-    const translateZ = scrollPosition * -0.5; // Ajusta este valor para cambiar la intensidad
-    textElement.style.transform = `translate(-50%, -50%) translateZ(${translateZ}px)`;
-});
